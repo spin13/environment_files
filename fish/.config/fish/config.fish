61fb77786845370fc5cd3be3cf114aae2da6035d
spin13 <pubspn13@gmail.com>
2019-01-26 15:44:38 +0900

    a

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,1 @@
-alias 'vi'='vim'
-alias 'git'='/usr/local/bin/git'
-set -U fish_user_paths $fish_user_paths $HOME/.rbenv/bin
-set -U fish_user_paths $fish_user_paths $HOME/.nodebrew/current/bin
-
 source ~/.config/fish/ssh_agent_start.fish
